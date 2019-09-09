a86032c2df62784efee133211ef3203f060c1c0d
Wayne Roddy <dubrod@icloud.com>
2019-09-09 09:41:51 -0500

    Create node_export.php
    paste in node export array to import

---
+++ node_export.php
@@ -0,0 +1,97 @@
+<?php
+$nodes = 
+array(
+  (object) array(
+      'vid' => '185',
+      'uid' => '139',
+      'title' => 'MODEL 1895 DARK SERIES',
+      'log' => '',
+      'status' => '1',
+      'comment' => '1',
+      'promote' => '0',
+      'sticky' => '0',
+      'vuuid' => '208b7c1d-6ecd-44bd-bc49-4e331deb2af7',
+      'nid' => '185',
+      'type' => 'product_display',
+      'language' => 'und',
+      'created' => '1555957711',
+      'changed' => '1562608553',
+      'tnid' => '0',
+      'translate' => '0',
+      'rh_action' => NULL,
+      'rh_redirect' => NULL,
+      'rh_redirect_response' => NULL,
+      'uuid' => '57e49317-0986-47a1-818d-221f2be02dbd',
+      'revision_timestamp' => '1562608553',
+      'revision_uid' => '139',
+      'title_field' => array(
+        'und' => array(
+          array(
+            'value' => 'MODEL 1895 DARK SERIES',
+            'format' => NULL,
+            'safe_value' => 'MODEL 1895 DARK SERIES',
+          ),
+        ),
+      ),
+      'field_product' => array(
+        'und' => array(
+          array(
+            'product_id' => '69',
+          ),
+        ),
+      ),
+      'field_product_category' => array(
+        'und' => array(
+          array(
+            'tid' => '5',
+          ),
+        ),
+      ),
+      'field_product_catalog_image' => array(
+        'und' => array(
+          array(
+            'fid' => '317',
+            'uid' => '139',
+            'filename' => '70455_Model-1895-Dark_Rifle_Right-Profile_3.png',
+            'uri' => 'public://70455_Model-1895-Dark_Rifle_Right-Profile_3.png',
+            'filemime' => 'image/png',
+            'filesize' => '136447',
+            'status' => '1',
+            'timestamp' => '1562608553',
+            'uuid' => '0a8e8e41-bca1-4909-b062-ab27f27f5de7',
+            'alt' => '',
+            'title' => '',
+            'width' => '723',
+            'height' => '271',
+          ),
+        ),
+      ),
+      'field_additional_copy' => array(
+        'und' => array(
+          array(
+            'value' => 'The speed .... optics.',
+            'format' => NULL,
+            'safe_value' => 'The ....optics.',
+          ),
+        ),
+      ),
+      'field_additional_copy_heading' => array(),
+      'title_original' => 'MODEL 1895',
+      'path' => array(
+        'pid' => '805',
+        'source' => 'node/185',
+        'alias' => 'lever-action/model-1895-big-bore/model-1895-dark-series',
+        'language' => 'und',
+      ),
+      'cid' => '0',
+      'last_comment_timestamp' => '1555957711',
+      'last_comment_name' => NULL,
+      'last_comment_uid' => '139',
+      'comment_count' => '0',
+      'name' => 'wayneroddy',
+      'picture' => '0',
+      'data' => 'a:6:{s:16:"ckeditor_default";s:1:"t";s:20:"ckeditor_show_toggle";s:1:"t";s:14:"ckeditor_width";s:4:"100%";s:13:"ckeditor_lang";s:2:"en";s:18:"ckeditor_auto_lang";s:1:"t";s:17:"mimemail_textonly";b:0;}',
+      'menu' => NULL,
+      'node_export_drupal_version' => '7',
+    )
+);
